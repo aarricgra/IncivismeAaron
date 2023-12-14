48be8e59146271d2d1f20b9ea57e6245fe9c011c
aarricgra <aarricgra@alu.edu.gva.es>
2023-12-14 14:59:02 +0100

    1

--- app/src/main/java/com/example/incivismeaaron/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/incivismeaaron/ui/home/HomeFragment.java
@@ -2,8 +2,14 @@ package com.example.incivismeaaron.ui.home;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +28,13 @@ import com.example.incivismeaaron.databinding.FragmentHomeBinding;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
@@ -30,6 +43,7 @@ public class HomeFragment extends Fragment {
     private Location mLastLocation;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
+
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
@@ -87,17 +101,68 @@ public class HomeFragment extends Fragment {
             mFusedLocationClient.getLastLocation().addOnSuccessListener(
                     location -> {
                         if (location != null) {
-                            mLastLocation = location;
-                            binding.tLocal.setText(
-                                    String.format("Latitud: %1$.4f \n Longitud: %2$.4f\n Hora: %3$tr",
-                                            mLastLocation.getLatitude(),
-                                            mLastLocation.getLongitude(),
-                                            mLastLocation.getTime()));
+                            fetchAddress(location);
                         } else {
                             binding.tLocal.setText("Sense localització coneguda");
                         }
                     });
         }
+    }
+
+    private void fetchAddress(Location location) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        Geocoder geocoder = new Geocoder(requireContext(),
+                Locale.getDefault());
+
+        executor.execute(() -> {
+            // Aquest codi s'executa en segon pla
+            List<Address> addresses = null;
+            String resultMessage = "";
+
+            try {
+                addresses = geocoder.getFromLocation(
+                        location.getLatitude(),
+                        location.getLongitude(),
+                        // En aquest cas, sols volem una única adreça:
+                        1);
+
+
+                if (addresses == null || addresses.size() == 0) {
+                    if (resultMessage.isEmpty()) {
+                        resultMessage = "No s'ha trobat cap adreça";
+                        Log.e("INCIVISME", resultMessage);
+                    }
+                } else {
+                    Address address = addresses.get(0);
+                    ArrayList<String> addressParts = new ArrayList<>();
+
+                    for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                        addressParts.add(address.getAddressLine(i));
+                    }
+
+                    resultMessage = TextUtils.join("\n", addressParts);
+                    String finalResultMessage = resultMessage;
+                    handler.post(() -> {
+                        // Aquest codi s'executa en primer pla.
+                        binding.tLocal.setText(String.format(
+                                "Direcció: %1$s \n Hora: %2$tr",
+                                finalResultMessage, System.currentTimeMillis()));
+                    });
+                }
+
+            } catch (IOException ioException) {
+                resultMessage = "Servei no disponible";
+                Log.e("INCIVISME", resultMessage, ioException);
+            } catch (IllegalArgumentException illegalArgumentException) {
+                resultMessage = "Coordenades no vàlides";
+                Log.e("INCIVISME", resultMessage + ". " +
+                        "Latitude = " + location.getLatitude() +
+                        ", Longitude = " +
+                        location.getLongitude(), illegalArgumentException);
+            }
+        });
     }
 
     @Override
